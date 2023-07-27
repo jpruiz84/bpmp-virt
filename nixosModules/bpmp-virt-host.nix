7c6a660ddd595c577950def23b070407af3f2dc9
Julius Koskela <julius.koskela@unikie.com>
2023-07-28 00:39:35 +0300

    Test applying device tree inside Nix
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- nixosModules/bpmp-virt-host.nix
+++ nixosModules/bpmp-virt-host.nix
@@ -1,9 +1,23 @@
 # Copyright 2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{lib, ...}: {
+{ config, lib, ... }:
+
+let
+  bpmpHostProxyOverlay = (final: prev: {
+    bpmp_host_proxy = prev // {
+      compatible = "nvidia,bpmp-host-proxy";
+      allowed-clocks = [ "TEGRA234_CLK_UARTA" "TEGRA234_CLK_PLLP_OUT0" ];
+      allowed-resets = [ "TEGRA234_RESET_UARTA" ];
+      status = "okay";
+    };
+  });
+in
+
+{
   imports = [
     ./bpmp-virt-common.nix
   ];
+
   boot.kernelPatches = [
     {
       name = "BPMP virt enable host";
@@ -13,5 +27,9 @@
       };
     }
   ];
-  hardware.deviceTree.overlays.dtsFile = ./bpmp-virt.dts;
+
+  # Apply the device tree overlay
+  hardware.deviceTree.overlays = [ bpmpHostProxyOverlay ];
+
+  # The rest of your NixOS configuration...
 }
