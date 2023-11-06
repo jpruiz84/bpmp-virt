e7869a84d833e191450988ba1c5fd7c6446fa242
Julius Koskela <me@juliuskoskela.dev>
2023-11-06 19:07:07 +0200

    Correct qemu patch name
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

--- overlays/qemu/default.nix
+++ overlays/qemu/default.nix
@@ -4,7 +4,7 @@
       prev.patches
       ++ [
         # FIXME: This patch doesn't work with the qemu version from Nixpkgs.
-        ./patches/qemu-v8.1.0_bpmp-virt.patch
+        ./patches/0001-qemu-v8.1.0_bpmp-virt.patch
       ];
   });
 })
